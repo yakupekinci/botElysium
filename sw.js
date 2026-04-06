142dc929ea6e6cf4f61c75b4ec594a77eb7e30e4
Yakup Ekinci <99822215+yakupekinci@users.noreply.github.com>
2026-04-06 17:38:00 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,15 @@
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-const firebaseConfig = {
-  // BURAYA 1. ADIMDA ALDIĞIN CONFIG'İ YAPIŞTIR
-  apiKey: "YOUR_API_KEY",
-  projectId: "YOUR_PROJECT_ID",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
-
+  const firebaseConfig = {
+    apiKey: "AIzaSyCU08n2MbhgejPb8exKyvcPKn-xLkBu0sY",
+    authDomain: "botelysium-ddf50.firebaseapp.com",
+    projectId: "botelysium-ddf50",
+    storageBucket: "botelysium-ddf50.firebasestorage.app",
+    messagingSenderId: "763753788539",
+    appId: "1:763753788539:web:62715bac995c0dc5b45ede",
+    measurementId: "G-7GMTH40N5E"
+  };
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
